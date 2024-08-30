2d6f1b9e0a1b8d998b0b237486ab03300de2ab26
Ahmed <ahmedfdez93@gmail.com>
2024-08-30 12:14:06 -0400

    Add React components for main skeleton of the UI and custom css rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Row, Col } from "react-bootstrap"
+import { RandomRecipes } from "./components/Random"
+import styles from './assets/styles/main.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container fluid style={{backgroundColor: 'F5F5F5', height: '100vh'}}>
+        <Row>
+          <Col md={2} style={{backgroundColor: '#95CD41', height: '100vh'}} className={styles['aside-column']} justify-content-center>
+              {/* Aside column */}
+                Aside
+          </Col>
+
+          {/* Main Content */}
+          <Col>
+          <RandomRecipes />
+          </Col>
+        </Row>
+        </Container>
+        
     </>
   )
 }
---
+++ src/components/Random.jsx
@@ -0,0 +1,13 @@
+import {Row, Col} from 'react-bootstrap';
+
+export const RandomRecipes = () => {
+    return (
+        <>
+                <Row>
+                    <Col>
+                        <h1>Popular Recipes</h1>
+                    </Col>
+                </Row> 
+        </>
+    )
+}
