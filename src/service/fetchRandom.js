ce635f4c9bd7c9137266009ef8a2ace4cf5df1c0
Ahmed <ahmedfdez93@gmail.com>
2024-09-02 20:17:11 -0400

    Add svg´s in the aside column and search bar component

--- src/service/fetchRandom.js
+++ src/service/fetchRandom.js
@@ -5,7 +5,7 @@ export const fetchRandomRecipes = async () => {
         const response = await fetch(`${API.URL}/random?apiKey=${API.KEY}&number=${Math.floor(Math.random() * 20 + 1)}`)
         if(!response.ok) throw new Error('Something went wrong!')
         const data = await response.json()
-        return data
+        return data.recipes
     } catch (error) {
         console.log('Error: ', error);
     }
