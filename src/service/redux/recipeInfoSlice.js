3cda65bc92e6f9841a60aeb1c5a8c1e75598802a
Ahmed <ahmedfdez93@gmail.com>
2024-09-13 00:03:24 -0400

    Add functionality for get information about an specific recipe

---
+++ src/service/redux/recipeInfoSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { API } from "../api/api_credentials";
+
+export const fetchRecipeInfo = createAsyncThunk(
+    'recipeInfo/fetchRecipeInfo',
+    async (recipeId) => {
+        try {
+            const response = await fetch(`${API.URL}/${recipeId}/information?apiKey=${API.KEY}&includeNutrition=false`)
+            const info = await response.json()
+            console.log(info);
+            console.log(typeof info);
+            
+            return info
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            
+        }
+    }
+)
+
+const initialState = {
+    status: 'idle',
+    value: []
+}
+
+const recipeInfoSlice = createSlice({
+    name: 'recipeInfo',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchRecipeInfo.pending, (state) => {
+                state.status = 'loading'
+            } )
+            .addCase(fetchRecipeInfo.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.value.push(action.payload)
+            })
+            .addCase(fetchRecipeInfo.rejected, (state) => {
+                state.status = 'failed'
+            })
+    }
+})
+
+export default recipeInfoSlice.reducer
--- src/service/redux/store.js
+++ src/service/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import randomRecipesReducer from "./randomRecipesSlice";
 import inputSearchReducer from "./inputSearchSlice";
 import recipeSeekerReducer from "./recipeSeekerSlice";
+import recipeInfoReducer from "./recipeInfoSlice";
 
 const store = configureStore({
     reducer: {
         randomRecipes: randomRecipesReducer,
         inputSearch: inputSearchReducer,
-        recipeSeeker: recipeSeekerReducer
+        recipeSeeker: recipeSeekerReducer,
+        recipeInfo: recipeInfoReducer
     }
 })
 
